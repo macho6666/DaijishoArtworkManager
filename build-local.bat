4d49616cff37898ea30792e177530139db1d8158
macho6666 <macho0514@gmail.com>
2026-09-24 23:04:13 +0900

    Delete build-local.bat

--- build-local.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-python -m pip install --upgrade pip
-pip install -r requirements.txt
-pyinstaller --noconfirm --clean --onefile --windowed --name DaijishoArtworkManager --collect-all PIL --collect-all webview app.py
-pause
