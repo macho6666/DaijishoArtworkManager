b3ada27f4e53b307c0426b9bf5c26be6446dfce5
macho6666 <macho0514@gmail.com>
2026-09-24 22:28:38 +0900

    Delete build.bat

--- build.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-chcp 65001 >nul
-python -m pip install -r requirements.txt
-python -m PyInstaller --noconfirm --clean --onefile --windowed --name DaijishoArtworkManager app.py
-echo.
-echo EXE: dist\DaijishoArtworkManager.exe
-pause
